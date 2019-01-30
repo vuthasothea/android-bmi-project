53524a22cca3b9d6a039c6460e0a24cb1b3b4552
Vutha Sothea <vuthasothea@gmail.com>
2019-01-30 21:00:47 +0700

    already calculate BMI

--- app/src/main/java/app/sothea/com/bmiapplication/CalculateFragment.java
+++ app/src/main/java/app/sothea/com/bmiapplication/CalculateFragment.java
@@ -7,7 +7,11 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 
 /**
@@ -16,6 +20,10 @@ import android.widget.TextView;
 public class CalculateFragment extends Fragment {
 
     private TextView textResultNumber;
+    private TextView textResultText;
+    private EditText edtHeight;
+    private EditText edtWeight;
+    private Button btnCalculate;
 
     public CalculateFragment() {
         // Required empty public constructor
@@ -27,9 +35,50 @@ public class CalculateFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_calculate, container, false);
 
         textResultNumber = view.findViewById(R.id.text_result_number);
+        textResultText = view.findViewById(R.id.text_result_text);
+        edtHeight = view.findViewById(R.id.edt_height);
+        edtWeight = view.findViewById(R.id.edt_weight);
+        btnCalculate = view.findViewById(R.id.btnCalculate);
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateBmi();
+            }
+        });
+
         textResultNumber.setText("23.00kg/m\u00B2");
 
         return view;
+    }
+
+    public void calculateBmi() {
+        String strHeight = edtHeight.getText().toString();
+        String strWeight = edtWeight.getText().toString();
+        if(strHeight != null && !strHeight.equals("") && strWeight != null && !strWeight.equals("")) {
+            float height = Float.parseFloat(strHeight);
+            float weight = Float.parseFloat(strWeight);
+            float bmi = round((weight / (height * height)) * 10000, 1);
+            displayBmi(bmi);
+        }
+    }
+
+    public void displayBmi(float bmi) {
+        textResultNumber.setText(String.format("%.1f kg/m²", bmi));
+        if(bmi < 18.5) {
+            textResultText.setText("Underweight");
+        } else if(bmi < 25) {
+            textResultText.setText("Normal weight");
+        } else if(bmi < 30) {
+            textResultText.setText("Overweight");
+        } else {
+            textResultText.setText("Obesity");
+        }
+    }
+
+    public float round(float number, int digit) {
+        BigDecimal bd = new BigDecimal(number);
+        bd = bd.setScale(digit, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
     }
 
 }
